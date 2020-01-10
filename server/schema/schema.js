1bd007842257006b63aaa3f7df764dd4d745de98
ahmad4hat <rappar.stand@gmail.com>
2020-01-11 02:46:59 +0600

    add book mutation added

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,7 @@
 const grahql = require("graphql");
 const _ = require("lodash");
+const Book = require("./../models/book");
+const Author = require("./../models/author");
 
 const {
   GraphQLObjectType,
@@ -11,20 +13,20 @@ const {
 } = grahql;
 
 //dummy data
-let books = [
-  { name: "Name of the Wind", genre: "Fantasy", id: "1", authorId: "1" },
-  { name: "The Final Empire", genre: "Fantasy", id: "2", authorId: "2" },
-  { name: "The Hero of Ages", genre: "Fantasy", id: "4", authorId: "2" },
-  { name: "The Long Earth", genre: "Sci-Fi", id: "3", authorId: "3" },
-  { name: "The Colour of Magic", genre: "Fantasy", id: "5", authorId: "3" },
-  { name: "The Light Fantastic", genre: "Fantasy", id: "6", authorId: "3" }
-];
+// let books = [
+//   { name: "Name of the Wind", genre: "Fantasy", id: "1", authorId: "1" },
+//   { name: "The Final Empire", genre: "Fantasy", id: "2", authorId: "2" },
+//   { name: "The Hero of Ages", genre: "Fantasy", id: "4", authorId: "2" },
+//   { name: "The Long Earth", genre: "Sci-Fi", id: "3", authorId: "3" },
+//   { name: "The Colour of Magic", genre: "Fantasy", id: "5", authorId: "3" },
+//   { name: "The Light Fantastic", genre: "Fantasy", id: "6", authorId: "3" }
+// ];
 
-let authors = [
-  { name: "Patrick Rothfuss", age: 44, id: "1" },
-  { name: "Brandon Sanderson", age: 42, id: "2" },
-  { name: "Terry Pratchett", age: 66, id: "3" }
-];
+// let authors = [
+//   { name: "Patrick Rothfuss", age: 44, id: "1" },
+//   { name: "Brandon Sanderson", age: 42, id: "2" },
+//   { name: "Terry Pratchett", age: 66, id: "3" }
+// ];
 
 const BookType = new GraphQLObjectType({
   name: "Book",
@@ -37,7 +39,7 @@ const BookType = new GraphQLObjectType({
       type: AuthorType,
       resolve(parent, args) {
         console.log(parent);
-        return _.find(authors, { id: parent.authorId });
+        //return _.find(authors, { id: parent.authorId });
       }
     }
   })
@@ -53,7 +55,7 @@ const AuthorType = new GraphQLObjectType({
     books: {
       type: new GraphQLList(BookType),
       resolve(parent, args) {
-        return _.filter(books, { authorId: parent.id });
+        //return _.filter(books, { authorId: parent.id });
       }
     }
   })
@@ -68,7 +70,7 @@ const RootQuery = new GraphQLObjectType({
       type: BookType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return _.find(books, { id: args.id });
+        //return _.find(books, { id: args.id });
         //code to get the data from the db
       }
     },
@@ -76,22 +78,44 @@ const RootQuery = new GraphQLObjectType({
       type: AuthorType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return _.find(authors, { id: args.id });
+        //return _.find(authors, { id: args.id });
       }
     },
     books: {
       type: new GraphQLList(BookType),
       resolve(parent, args) {
-        return books;
+        //return books;
       }
     },
     authors: {
       type: new GraphQLList(AuthorType),
-      resolve: (parent, args) => authors
+      resolve: (parent, args) => {}
     }
   }
 });
 
+//mutation
+const Mutation = new GraphQLObjectType({
+  name: "Mutation",
+  fields: () => ({
+    addAuthor: {
+      type: AuthorType,
+      args: {
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt }
+      },
+      resolve: (parent, args) => {
+        let author = new Author({
+          name: args.name,
+          age: args.age
+        });
+        return author.save();
+      }
+    }
+  })
+});
+
 module.exports = new GraphQLSchema({
-  query: RootQuery
+  query: RootQuery,
+  mutation: Mutation
 });
--- server/server.js
+++ server/server.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/graphqldb");
+mongoose.connection.once("open", () =>
+  console.log("connected to the database")
+);
 
 const app = express();
 
