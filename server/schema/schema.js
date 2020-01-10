f630f3221a29bff17fd11e4f376ccba540eb8221
ahmad4hat <rappar.stand@gmail.com>
2020-01-11 01:52:30 +0600

    authors and books root query added

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -78,6 +78,16 @@ const RootQuery = new GraphQLObjectType({
       resolve(parent, args) {
         return _.find(authors, { id: args.id });
       }
+    },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args) {
+        return books;
+      }
+    },
+    authors: {
+      type: new GraphQLList(AuthorType),
+      resolve: (parent, args) => authors
     }
   }
 });
